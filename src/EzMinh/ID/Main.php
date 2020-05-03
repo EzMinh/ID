b7f6a8c532be2681e669373ffd79367febaeb468
EzMinh <64187662+EzMinh@users.noreply.github.com>
2020-05-03 16:52:46 +0700

    Update Files
    Update Main.php, plugin.yml and upload config.yml

--- src/EzMinh/ID/Main.php
+++ src/EzMinh/ID/Main.php
@@ -7,21 +7,35 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\Player;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
     public function onEnable() {
-        $this->getLogger()->info(C::GREEN."Đã bật");
-    }
-    public function onDisable() {
-        $this->getLogger()->info(C::RED."Đã tắt");
+        @mkdir($this->getDataFolder());
+        $this->saveDefaultConfig();
+        $this->getResource("config.yml");
     }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool {
         if(!$sender instanceof Player) {
-            $sender->sendMessage(C::RED."Bạn không thể dùng lệnh tại đây");
+            if($this->getConfig()->get("lang") == "en")
+            {
+                $sender->sendMessage(C::RED."You can't not use this command here!");
+            }
+            else
+            {
+                $sender->sendMessage(C::RED."Bạn không thể dùng lệnh tại đây !");
+            }
         } else {
             if($cmd->getName() == "id") {
                 $id_item = $sender->getInventory()->getItemInHand()->getId();
+                if($this->getConfig()->get("lang") == "en")
+            {
+                $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
+            }
+            else
+            {
                 $sender->sendMessage("ID của vật phẩm bạn đang cầm là: ".C::YELLOW.$id_item);
+            }
             }
         }
         return true;
