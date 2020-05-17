932724c4fb0459eabb0a51b57c51489c663e3b7f
EzMinh <64187662+EzMinh@users.noreply.github.com>
2020-05-17 20:16:53 +0700

    Upload Files
    Update plugin.yml, README.md and Main.php

--- src/EzMinh/ID/Main.php
+++ src/EzMinh/ID/Main.php
@@ -17,25 +17,31 @@ class Main extends PluginBase {
     }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool {
         if(!$sender instanceof Player) {
-            if($this->getConfig()->get("lang") == "en")
-            {
+            switch($this->getConfig()->get("lang")) {
+                case "en":
+                    $sender->sendMessage(C::RED."You can't not use this command here!");
+                break;
+                case "vi":
+                    $sender->sendMessage(C::RED."Bạn không thể dùng lệnh tại đây !");
+                break;
+                default:
                 $sender->sendMessage(C::RED."You can't not use this command here!");
-            }
-            else
-            {
-                $sender->sendMessage(C::RED."Bạn không thể dùng lệnh tại đây !");
+                break;
             }
         } else {
             if($cmd->getName() == "id") {
                 $id_item = $sender->getInventory()->getItemInHand()->getId();
-                if($this->getConfig()->get("lang") == "en")
-            {
-                $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
-            }
-            else
-            {
-                $sender->sendMessage("ID của vật phẩm bạn đang cầm là: ".C::YELLOW.$id_item);
-            }
+                switch($this->getConfig()->get("lang")) {
+                    case "en":
+                        $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
+                    break;
+                    case "vi":
+                        $sender->sendMessage("ID của vật phẩm bạn đang cầm là: ".C::YELLOW.$id_item);
+                    break;
+                    default:
+                    $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
+                    break;
+                }
             }
         }
         return true;
