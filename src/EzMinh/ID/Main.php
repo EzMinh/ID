52aa070ba72d2a574f33962d4ef135d4640e334d
HiToLaKhanh <63938720+HiToLaKhanh@users.noreply.github.com>
2020-05-17 16:44:10 +0700

    fix something

--- src/EzMinh/ID/Main.php
+++ src/EzMinh/ID/Main.php
@@ -12,30 +12,42 @@ use pocketmine\utils\Config;
 class Main extends PluginBase {
     public function onEnable() {
         @mkdir($this->getDataFolder());
-        $this->saveDefaultConfig();
+        $this->saveDefaultConfig(); 
         $this->getResource("config.yml");
     }
     public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool {
         if(!$sender instanceof Player) {
-            if($this->getConfig()->get("lang") == "en")
-            {
-                $sender->sendMessage(C::RED."You can't not use this command here!");
-            }
-            else
-            {
-                $sender->sendMessage(C::RED."Bạn không thể dùng lệnh tại đây !");
+            switch($this->getConfig()->get("lang")){
+                case "en":
+                    $this->getLogger()->info(C::RED."You can't not use this command here!");
+                break;
+
+                case "vi":
+                    $this->getLogger()->info(C::RED."Bạn không thể dùng lệnh tại đây !");
+                break;
+
+                default:
+                    $this->getLogger()->info(C::RED."Bạn không thể dùng lệnh tại đây !");
+                    $this->getLogger()->info(C::RED."You can't not use this command here!");
+                break;
             }
         } else {
-            if($cmd->getName() == "id") {
+            if($this->getConfig()->get("lang")) {
                 $id_item = $sender->getInventory()->getItemInHand()->getId();
-                if($this->getConfig()->get("lang") == "en")
-            {
-                $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
-            }
-            else
-            {
-                $sender->sendMessage("ID của vật phẩm bạn đang cầm là: ".C::YELLOW.$id_item);
-            }
+                switch($this->config->get()){
+                    case "en":
+                        $sender->sendMessage("The ID of the item you are holding is: ".C::YELLOW.$id_item);
+                    break;
+
+                    case "vi":
+                        $sender->sendMessage("ID của vật phẩm bạn đang cầm là: ".C::YELLOW.$id_item);
+                    break;
+
+                    default:
+                        $sender->sendMessage(C::RED."Please switch your language to english or vietnamese!");
+                        $sender->sendMessage(C::RED."Hãy chỉnh lại ngôn ngữ sang tiếng anh hoặc tiếng việt!");
+                    break;
+                }
             }
         }
         return true;
